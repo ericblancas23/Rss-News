7435460bb1c353122fb68e949f849cc2b5a7fee8
EricB <ericblancas23@gmail.com>
2019-05-14 00:15:12 -0700

    serializers

--- rssreader/rss/views.py
+++ rssreader/rss/views.py
@@ -1,14 +1,27 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .serializers import FeedSerializer
+from .models import Feed
 import feedparser
 
-# Create your views here.
-# def index(request):
-#     url = 'https://www.djangoproject.com/rss/weblog/'
-#     feed = feedparser.parse(url)
-#     return render(request, 'rss/reader.html', {
-#         'feed':feed
-#     })
+def rest_feeds(request):
+    if request.method == "GET":
+        feeds = Feed.objects.all()
+        serializer = FeedSerializer(feeds, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = FeedSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
 def index(request):
    url = 'https://www.djangoproject.com/rss/weblog/'
    feed = feedparser.parse(url)
