7e8cf818a61e646e0f1819f30e6a3a4a9f767c54
EricB <ericblancas23@gmail.com>
2019-05-15 13:35:28 -0700

    reinitialized urls

--- rssreader/rss/urls.py
+++ rssreader/rss/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('feeds/', views.rest_feeds, name='rest-feeds'),
+    re_path(r'^feeds/(?P<pk>[0-9]+)/$', views.rest_feeds_detail, name='rest_feeds_detail'),
+    path('items/', views.rest_items, name='rest_items')
 ]
--- rssreader/rss/views.py
+++ rssreader/rss/views.py
@@ -8,6 +8,7 @@ from .serializers import FeedSerializer
 from .models import Feed
 import feedparser
 
+@csrf_exempt
 def rest_feeds(request):
     if request.method == "GET":
         feeds = Feed.objects.all()
@@ -21,6 +22,43 @@ def rest_feeds(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def rest_feed_detail(request, pk):
+    try:
+        feed = Feed.objects.get(pk=pk)
+    except Feed.DoesNotExist:
+        return HttpResponse(status=404)
+    if request.method == "GET":
+        serializer = FeedSerializer(feed)
+        return JsonResponse(serializer.data)
+    elif request.method == "POST":
+        data = JSONParser().parse(request)
+        serializer = FeedSerializer(feed, data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=404)
+    elif request.method == "DELETE":
+        feed.delete()
+        return HttpResponse()
+
+def rest_items(request):
+    feeds = Feed.objects.all()
+    items = []
+
+    for feed in feeds:
+        rss = feedparser.parse(feed.url)
+
+        try: 
+            items.extend(rss["items"])
+        except KeyError:
+            continue
+
+        items = list(reversed(sorted(items, key=lambda item: item["published_parse"])))
+
+        return JsonResponse(items, safe=False)
 
 def index(request):
    url = 'https://www.djangoproject.com/rss/weblog/'
